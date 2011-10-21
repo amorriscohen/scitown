e8e0a7becea786a663526242dfae25b10030b045
Adam Morris-Cohen <amorriscohen@gmail.com>
2011-10-20 22:55:32 -0500

    Initial commit

---
+++ scitown/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scitown::Application.config.secret_token = 'ca4a674c4602e6a398814d33fb97b9689cc0bd742a43f832a9d82d9f572cb6947defb990d31c16a5c2cd1ced2b08d0720f045491998c1ae88d7b5774fdafbe27'
